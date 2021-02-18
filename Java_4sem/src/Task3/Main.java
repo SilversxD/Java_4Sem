562069ada13506a5a47130e19d321324c5df3871
SilversxD <silverscjserge@gmail.com>
2021-02-18 21:23:21 +0300

    Comminig again first task (crashed) and done 2,3

---
+++ Java_4sem/src/Task3/Main.java
@@ -0,0 +1,12 @@
+package Task3;
+
+import Task3.Test.ListSyncTest;
+import Task3.Test.MapSyncTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MapSyncTest.test();
+        ListSyncTest.test();
+    }
+}
